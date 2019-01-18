368b623d60bdc83a96cb47cb4a0076ee22b1321b
smeerws <hello@smeerws.at>
2019-01-18 15:26:40 +0100

    added Folder Prefabs, Scripts; renamed scenes; implemented SceneLoader script and hooked it up on Empty Object (after rename SceneLoader) assigned to start button, scene next button and restart button at OnClick the functions form the SceneLoader script.

---
+++ GitCommitSuicide/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    enum Scenes { Start, Main, End };
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Start);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene((int)Scenes.Main);
+    }
+
+    public void LoadEndScene()
+    {
+        SceneManager.LoadScene((int)Scenes.End);
+    }
+    
+}
