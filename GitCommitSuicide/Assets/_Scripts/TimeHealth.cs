8979d6c4027c60517bd7f22e671b3bf14191a44d
chiini <chiini@hotmail.de>
2019-01-30 11:18:55 +0100

    started playerLife + Timer stop

---
+++ GitCommitSuicide/Assets/_Scripts/TimeHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using TMPro;
+
+public class TimeHealth : MonoBehaviour
+{
+
+    public int timeLeft = 60;
+    public TextMeshProUGUI countdown;
+
+    public int playerLife = 3;
+    public TextMeshProUGUI lifeCount;
+
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+
+    }
+
+    void Update()
+    {
+        lifeCount.text = ("Lives: " + playerLife);
+        countdown.text = ("Time: " + timeLeft);
+
+        checkLife();
+    }
+
+    void checkLife()
+    {
+        if(timeLeft == 0)
+        {
+            StopCoroutine("LoseTime");
+            //playerLife--;
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
